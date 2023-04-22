0c5b7e55ef69297e6712b98cd73c8555957577a8
posydomoika <omfgwow76@gmail.com>
2023-04-22 17:46:55 +0300

    refactor code

--- data_preparation.py
+++ data_preparation.py
@@ -6,6 +6,8 @@ from PIL import Image
 from tensorflow.keras.applications import VGG19
 
 vgg19_cl = VGG19()
+
+
 def processed_image(img):
     image = conver_good_size(img)
     size = image.shape
@@ -15,17 +17,18 @@ def processed_image(img):
     ab /= 128
     X = X.reshape(1, size[0], size[1], 1)
     ab = ab.reshape(1, size[0], size[1], 2)
-    return X, ab, np.array(image,dtype=int)
+    return X, ab, np.array(image, dtype=int)
 
 
 def create_data_imagenet():
     ph = Image.open("i1.jpg")
     x, ab, rgb = processed_image(ph)
-    a = np.zeros((1,224,224,3))
-    a[0,:,:,0] = x[0,:,:,0]
-    a[0,:,:,1] = x[0,:,:,0]
-    a[0,:,:,2] = x[0,:,:,0]
-    return x,ab,rgb
+    a = np.zeros((1, 224, 224, 3))
+    a[0, :, :, 0] = x[0, :, :, 0]
+    a[0, :, :, 1] = x[0, :, :, 0]
+    a[0, :, :, 2] = x[0, :, :, 0]
+    return x, ab, rgb
+
 
 def conver_good_size(img):
     if not type(img) == Image:
@@ -38,10 +41,12 @@ def conver_good_size(img):
 
 def avg_photo():
     ph = np.array(ph)
-    ph = np.expand_dims(ph,axis=0)
-    avg = np.sum(ph,axis=3) / 3
-    avg.shape = (1,224,224,1)
-    return avg,ph
+    ph = np.expand_dims(ph, axis=0)
+    avg = np.sum(ph, axis=3) / 3
+    avg.shape = (1, 224, 224, 1)
+    return avg, ph
+
+
 def data_for_vgg(img):
     if not type(img) == Image:
         img = Image.fromarray(np.squeeze(img))
@@ -67,4 +72,3 @@ def lab_ab_in_rgb(lab: np.array, ab: np.array):
 if __name__ == '__main__':
     # create_data_imagenet()
     avg_photo()
-
